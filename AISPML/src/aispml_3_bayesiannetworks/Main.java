b3ac3c192b3496dd303dc751907350db4626a55c
Chris <Chris@192.168.0.12>
2015-05-23 20:09:54 +0200

    yo, alsjeblieft!

--- AISPML/src/aispml_3_bayesiannetworks/Main.java
+++ AISPML/src/aispml_3_bayesiannetworks/Main.java
@@ -5,6 +5,8 @@
  */
 package aispml_3_bayesiannetworks;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author harmbrand
@@ -29,11 +31,45 @@ public class Main {
         vars2[1] = Variable.D;
         double[] dubs2 = {0.2, 0.8, 0.9, 0.1};
         
+        Variable[] vars3 = new Variable[1];
+        vars3[0] = Variable.B;
+        double[] dubs3 = {0.1, 0.9};
+        
+        Variable[] vars4 = new Variable[1];
+        vars4[0] = Variable.D;
+        double[] dubs4 = {0.7, 0.3};
+        
+        ArrayList<Variable> varsAll = new ArrayList<>();
+        varsAll.add(Variable.A);
+        varsAll.add(Variable.B);
+        varsAll.add(Variable.C);
+        varsAll.add(Variable.D);
+        
         Factor fact = new Factor(vars1, dubs1);
         Factor fact2 = new Factor(vars2, dubs2);
+        Factor fact3 = new Factor(vars3, dubs3);
+        Factor fact4 = new Factor(vars4, dubs4);
         System.out.println(fact);
         System.out.println(fact2);
+        System.out.println(fact3);
+        System.out.println(fact4);
         
+        ArrayList<Factor> factAll = new ArrayList<>();
+        factAll.add(fact);
+        factAll.add(fact2);
+        factAll.add(fact3);
+        factAll.add(fact4);
+        
+        ArrayList<VariBool> given = new ArrayList<>();
+        given.add(new VariBool(Variable.B, true));
+        
+        Variable query = Variable.D;
+        
+        VariableElimination eli = new VariableElimination();
+        
+        Factor result = eli.elimination(varsAll, factAll, given, query);
+        System.out.println(result);
+        /*
         //Set given:
         Factor givenFact = fact.setGiven(Variable.A, true);
         System.out.println(givenFact);
@@ -51,6 +87,7 @@ public class Main {
         //Standardization:
         Factor standFact = sum2Fact.standardize();
         System.out.println(standFact);
+                */
     }
     
 }
--- AISPML/src/aispml_3_bayesiannetworks/VariableElimination.java
+++ AISPML/src/aispml_3_bayesiannetworks/VariableElimination.java
@@ -16,6 +16,7 @@ public class VariableElimination {
     
     public Factor elimination(ArrayList<Variable> vs, ArrayList<Factor> fs, ArrayList<VariBool> given, Variable query)
     {
+        System.out.println(given.get(0));
         for(int i=0; i< vs.size(); i++)
         {
             if(!vs.get(i).equals(query))
@@ -40,6 +41,7 @@ public class VariableElimination {
                             }
                             if(contains)
                             {
+                                System.out.println("it is found!");
                                 Factor newFact = fs.get(f).setGiven(given.get(g).getVar(), given.get(g).getBool());
                                 fs.add(newFact);
                                 fs.remove(f);
@@ -53,9 +55,50 @@ public class VariableElimination {
                 }
                 if(!isGiven)
                 {
-                    
+                    int f = 0;
+                    ArrayList<Factor> usedFactors = new ArrayList<>();
+                    while(f<fs.size())
+                    {
+                        Variable[] variables = fs.get(f).getVariables();
+                        boolean contains = false;
+                        for(int b=0; b<variables.length; b++)
+                        {
+                            if(variables[b].equals(vs.get(i)))
+                            {
+                                contains = true;
+                            }
+                        }
+                        if(contains)
+                        {
+                            usedFactors.add(fs.get(f));
+                            fs.remove(f);
+                        }
+                        else
+                        {
+                            f++;
+                        }
+                    }
+                    Factor multiFact = multiHelp(usedFactors);
+                    Factor summedFact = multiFact.desummation(vs.get(i));
+                    fs.add(summedFact);
                 }
             }
+        }
+        Factor finalFact = multiHelp(fs);
+        Factor normalFact = finalFact.standardize();
+        return normalFact;
+    }
+    
+    public Factor multiHelp(ArrayList<Factor> factors)
+    {
+        if(factors.size() <= 1)
+        {
+            return factors.get(0);
+        }
+        else
+        {
+            Factor fact = factors.remove(0);
+            return fact.multiplication(multiHelp(factors));
         }
     }
 }
