01965236264bdf805f231274cf5467715f73231f
Irma <Irma@192.168.1.109>
2015-05-23 19:55:35 +0200

    initialization

--- AISPML/src/aispml_3_bayesiannetworks/BayesNetwork.java
+++ AISPML/src/aispml_3_bayesiannetworks/BayesNetwork.java
@@ -18,24 +18,48 @@ class BayesNetwork
     public BayesNetwork()
     {
         
-        Variable[] vars1 = new Variable[2];
-        vars1[0] = Variable.A;
-        vars1[1] = Variable.B;
-        double[] dubs1 = {0.1, 0.9, 0.2, 0.8};        
-        Factor factor1 = new Factor(vars1, dubs1);
+        Variable[] varsA = new Variable[1];
+        varsA[0] = Variable.A;
+        double[] dubsA = {0.1, 0.9};        
+        Factor factorA = new Factor(varsA, dubsA);
         
-        Variable[] vars2 = new Variable[2];
-        vars2[0] = Variable.A;
-        vars2[1] = Variable.B;
-        double[] dubs2 = {0.3, 0.7, 0.6, 0.4};
-        Factor factor2 = new Factor(vars2, dubs2);
+        Variable[] varsB = new Variable[1];
+        varsB[0] = Variable.B;
+        double[] dubsB = {0.25, 0.75};        
+        Factor factorB = new Factor(varsB, dubsB);
         
-        Variable[] vars1x2 = new Variable[3];
-        vars1x2[0] = Variable.A;
-        vars1x2[1] = Variable.B;
-        vars1x2[2] = Variable.C;
-        double[] dubs1x2 = {0.3, 0.7, 0.4, 0.6, 0.2, 0.8, 0.5, 0.5};
-        Factor factor1x2 = new Factor(vars1x2, dubs1x2);
+        Variable[] varsC = new Variable[3];
+        varsC[0] = Variable.C;
+        varsC[0] = Variable.B;
+        varsC[0] = Variable.A;
+        double[] dubsC = {0.9, 0.1 , 0.7, 0.3, 0.5, 0.5 , 0.01, 0.99};        
+        Factor factorC = new Factor(varsC, dubsC);
         
+        Variable[] varsD = new Variable[2];
+        varsD[0] = Variable.D;
+        varsD[1] = Variable.C;
+        double[] dubsD = {0.35, 0.65, 0.2, 0.8};        
+        Factor factorD = new Factor(varsD, dubsD);
+        
+        Variable[] varsE = new Variable[2];
+        varsE[0] = Variable.E;
+        varsE[1] = Variable.C;
+        double[] dubsE = {0.5, 0.5, 0.5, 0.5};
+        Factor factorE = new Factor(varsE, dubsE);
+        
+        
+        
+        //      
+        //     ---                 ---
+        //    | A |               | B |
+        //     --- \             / ---
+        //           \         /
+        //              ---
+        //             | C |
+        //              ---
+        //            /      \
+        //     ---  /           \  ---
+        //    | D |               | E |
+        //     ---                 ---
     }
 }
